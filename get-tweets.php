d7e1aab519a3a4add6a362bf372be8792f4f7770
tony walt <tonyjwalt@gmail.com>
2013-07-21 11:18:14 -0600

    adjusted php to properly pull and parse results based on a search term

--- get-tweets.php
+++ get-tweets.php
@@ -2,7 +2,10 @@
 session_start();
 require_once("twitteroauth-master/twitteroauth/twitteroauth.php"); //Path to twitteroauth library
  
-// Account Variables
+///////////////
+// VARIABLES //
+///////////////
+//-- Acount Vairalbes --//
 $filename = 'keys.php';
 if (file_exists($filename)) {
     include $filename; //I'm using this file to store my keys so they aren't put on github
@@ -14,38 +17,45 @@ if (file_exists($filename)) {
 }
 $connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
 
-//Passed Variables
+//-- Passed Vairalbes --//
 $notweets = (isset($_GET['tweetNum'])) ? $_GET['tweetNum'] : 0;
 $cardWidth = (isset($_GET['cardWidth'])) ? $_GET['cardWidth'] : 300;
 $cardHeight = (isset($_GET['cardHeight'])) ? $_GET['cardHeight'] : 300;
 $term = (isset($_GET['term'])) ? $_GET['term'] : 'vine';
- 
+$searchType = (isset($_GET['searchType']) && $_GET['searchType'] == 'user') ? 'user' : 'term';
+
+/////////////
+// ACTIONS //
+/////////////
 //Get the Tweets	
-if ( isset($_GET['searchType']) && $_GET['searchType'] == 'user' ) {
+if ( $searchType == 'user' ) {
 	$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$term."&count=".$notweets);
 } else {
-	// This isn't parsing right. Need to examine this.
-	// echo json_encode ($tweets);
 	$tweets = $connection->get("https://api.twitter.com/1.1/search/tweets.json?q=".$term."&count=".$notweets);
 }
 
 //Parse the Tweets and Return the Vines
 $parser = new parseTweetData;
-echo $parser -> parseTweets($tweets, $cardWidth, $cardHeight);
+echo $parser -> parseTweets($searchType, $tweets, $cardWidth, $cardHeight);
 
-
-// FUNCTIONS
+///////////////
+// FUNCTIONS //
+///////////////
 function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
   $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
   return $connection;
 }
 
+/////////////
+// CLASSES //
+/////////////
 //Class to parse the tweet data for vines and put them in a list
 class parseTweetData {
-	public function parseTweets($tweets, $cardWidth, $cardHeight) {
+	public function parseTweets($tweetType, $tweets, $cardWidth, $cardHeight) {
 		$vineUL = '<ul class="vine-list">';
 		$vines = 0;
-		foreach ($tweets as $tweet) {
+		$toParse = ( $tweetType == 'user' ) ? $tweets : $tweets->statuses;
+		foreach ($toParse as $tweet) {
 			if ( isset($tweet->entities->urls[0]->expanded_url) ) {
 				// works in PHP, now needs to check that url for vine
 				$url = $tweet->entities->urls[0]->expanded_url;
